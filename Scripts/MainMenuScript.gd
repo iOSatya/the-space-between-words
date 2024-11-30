5e8163fe4087f1d971ed79ce2d8aac38e13402c0
iOSatya <125645326+iOSatya@users.noreply.github.com>
2024-11-30 13:27:35 +0700

    Added Save System

--- Scripts/MainMenuScript.gd
+++ Scripts/MainMenuScript.gd
@@ -2,10 +2,15 @@ extends Control
 
 func _on_NewGameButton_pressed():
 	# Load the first level of your game
-	get_tree().change_scene("res://Scenes/StoryScene.tscn")
+	Global.change_scene_with_save("res://Scenes/StoryScene.tscn")
 
 func _on_ContinueButton_pressed():
 	# Load saved game data (you'll need a save system for this)
+	var save_data = Global.load_game()
+	if save_data and "current_scene" in save_data:
+		get_tree().change_scene_to_file(save_data["current_scene"])
+	else:
+		print("No save data found!")
 	print("Continue button pressed!")  # Replace with actual loading logic
 
 func _on_QuitButton_pressed():
