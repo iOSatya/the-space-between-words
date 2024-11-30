a8f20030bf4c103ac6f8f76441d7b9ee44859eb6
M-Faizhal <faizhalmusthafa@gmail.com>
2024-11-30 16:01:16 +0700

    ChangeScene

--- Scripts/StoryScript.gd
+++ Scripts/StoryScript.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var animation_player : AnimationPlayer
 @export var autoplay : bool = false
+@export var next_scene : PackedScene
 
 func _input(event):
 	if event.is_action_pressed("next") and not animation_player.is_playing():
@@ -10,3 +11,6 @@ func _input(event):
 func pause():
 	if autoplay == false:
 		animation_player.pause()
+
+func change_scene():
+	get_tree().change_scene_to_packed(next_scene)
