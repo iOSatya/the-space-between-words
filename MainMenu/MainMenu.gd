26acefe1e32168e9ad7dc5b44af4eac1d2c19151
iOSatya <125645326+iOSatya@users.noreply.github.com>
2024-11-30 10:36:09 +0700

    Initial commit

---
+++ MainMenu/MainMenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_NewGameButton_pressed():
+	# Load the first level of your game
+	get_tree().change_scene("res://path_to_your_first_level.tscn")
+
+func _on_ContinueButton_pressed():
+	# Load saved game data (you'll need a save system for this)
+	print("Continue button pressed!")  # Replace with actual loading logic
+
+func _on_QuitButton_pressed():
+	# Quit the game
+	get_tree().quit()
