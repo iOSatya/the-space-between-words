5d004cb7466b6c2fe3346b47d3388d2d4c5cc229
iOSatya <125645326+iOSatya@users.noreply.github.com>
2024-11-30 12:25:59 +0700

    Update

--- Scripts/MainMenuScript.gd
+++ Scripts/MainMenuScript.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_NewGameButton_pressed():
 	# Load the first level of your game
-	get_tree().change_scene("res://path_to_your_first_level.tscn")
+	get_tree().change_scene("res://Scenes/StoryScene.tscn")
 
 func _on_ContinueButton_pressed():
 	# Load saved game data (you'll need a save system for this)
